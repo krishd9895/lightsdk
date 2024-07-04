e8c9e551191f94a587e23bd864a832a6ad4dfee7
krishd9895 <105974502+krishd9895@users.noreply.github.com>
2024-07-04 22:01:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from flask import Flask
-import threading
-from lsdk import lsdk as run_main_logic
+# from flask import Flask
+# import threading
+# from lsdk import lsdk as run_main_logic
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello!"
+# @app.route('/')
+# def hello():
+#     return "Hello!"
 
-def run_flask_app():
-    app.run(host='0.0.0.0', port=8080)
+# def run_flask_app():
+#     app.run(host='0.0.0.0', port=8080)
 
-if __name__ == '__main__':
-    # Start the main logic in a separate thread
-    main_thread = threading.Thread(target=run_main_logic)
-    main_thread.start()
+# if __name__ == '__main__':
+#     # Start the main logic in a separate thread
+#     main_thread = threading.Thread(target=run_main_logic)
+#     main_thread.start()
 
-    # Run the Flask app
-    run_flask_app()
+#     # Run the Flask app
+#     run_flask_app()
