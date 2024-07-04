133d9045ccdd6d37ae5f7a3b7eb8089954502dfc
krishd9895 <105974502+krishd9895@users.noreply.github.com>
2024-07-04 21:45:25 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import threading
+from main import main as run_main_logic
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello!"
+
+def run_flask_app():
+    app.run(host='0.0.0.0', port=8070)
+
+if __name__ == '__main__':
+    # Start the main logic in a separate thread
+    main_thread = threading.Thread(target=run_main_logic)
+    main_thread.start()
+
+    # Run the Flask app
+    run_flask_app()
